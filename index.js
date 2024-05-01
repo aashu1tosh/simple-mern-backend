a1c336efaedd9ad257c321f44d0bb84665293ddd
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-05-01 07:19:27 +0545

    updated

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get("/", (req, res) => {
 
 mongoose
   .connect(
-    "mongodb+srv://haris2iftikhar:GClTzr15XhkjvN6k@backenddb.nrurtot.mongodb.net/Node-API?retryWrites=true&w=majority"
+    "mongodb+srv://admin:admin@backenddb.ggsm4dn.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB"
   )
   .then(() => {
     console.log("Connected to database!");
